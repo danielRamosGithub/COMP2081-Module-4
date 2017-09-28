ece0435c76243a68cd4f8a05964e0e5702074a9d
Daniel C. Ramos <daniel@Daniels-MacBook-Pro.local>
2017-09-28 16:23:44 -0400

    Bootstrap navbar lesson

---
+++ lesson-files/Accordion/js/accordion.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  // hide all ULs inside LI.drawer except the first one
+    
+    // Add a click handler to the H2 elements
+
+        // hide all the drawer contents and then
+	   // remove the class .open from all H2 elements (previous DOM node to the UL)
+
+        // Add the class 'open' to the clicked H2 (this), then
+        // show the associated drawer content (next) with the slideDown() function
+
+    // end the click handler
+});
